da902c3dd538d9aad2c9ddfc8da3ba11d4aeb392
alfonsotis <58032826+alfonsotis@users.noreply.github.com>
2023-06-26 09:49:08 +0200

    Filter games by platform

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
+import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
 
 interface Props {
-    selectedGenre: Genre | null
+    selectedGenre: Genre | null;
+    selectedPlatform: Platform | null;
 }
 
 
-const GameGrid = ({ selectedGenre }: Props) => {
-    
-  const { data, error, isLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+
+  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
   const skeloetons = [1, 2, 3, 4, 5, 6];
 
   return (
