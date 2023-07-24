564e84c5f6ac820835e6a0802f36e92b431f3669
alfonsotis <58032826+alfonsotis@users.noreply.github.com>
2023-07-24 08:39:36 +0200

    use platforms use apiClient class

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
-import apiClient, { FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 
+
+const apiClient = new APIClient<Platform>('platforms/lists/parents');
 
 export interface Platform {
     id: number,
@@ -9,12 +11,12 @@ export interface Platform {
     slug: string;
 }
 
-const usePlatforms = () => useQuery({
+const usePlatforms = () => 
+useQuery({
     queryKey: ["platforms"],
-    queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents')
-        .then(res=>res.data.results),
-        staleTime: 24 * 60 * 60 * 1000, // 24h
-        initialData: platforms
+    queryFn: apiClient.getAll,
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: {count: platforms.length, next:null, results: platforms} || undefined,
 });
 
 
