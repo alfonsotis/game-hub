12ab6724153f1f71a90d890191f3c6cb411a397f
alfonsotis <58032826+alfonsotis@users.noreply.github.com>
2023-07-24 08:40:07 +0200

    useGenres use new apiClient

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
-import genres from "../data/genres";
 import { useQuery } from "@tanstack/react-query";
-import apiClient, { FetchResponse } from "../services/api-client";
+import genres from "../data/genres";
+import APIClient from "../services/api-client";
 
+const apiClient = new APIClient<Genre>('/genres')
 
 export interface Genre {
   id: number;
@@ -10,14 +10,13 @@ export interface Genre {
   image_background: string;
 }
 
- const useGenres = () => useQuery({
-  queryKey: ['genres'],
-  queryFn: () => 
-    apiClient.get<FetchResponse<Genre>>("/genres")
-    .then(res => res.data.results),
-    staleTime: 24 * 60 * 60 * 1000, // 24h
-    initialData: genres
-});
+ const useGenres = () => 
+  useQuery({
+      queryKey: ['genres'],
+      queryFn: apiClient.getAll,
+      staleTime: 24 * 60 * 60 * 1000, // 24h
+      initialData: {count: genres.length, results: genres, next:null},
+  });
 
 
 export default useGenres;
